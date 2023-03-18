9b073ae26b6b39c882592a98d981984e602ecaaf
vin <didekqls93@gmail.com>
2023-03-18 23:10:49 +0900

    commit message 11:10PM

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div>TypeScript</div>
+      <div>Git Hub Practice</div>
       <div>commit message 1</div>
+      <div>commit message 2 / 11:10 PM</div>
     </div>
   );
 }
